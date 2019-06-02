6a325d1566b0a9b5a50bb35b74ccb5fbf4e70365
Manojkumar Muralidharan <manoj.wolfpack@gmail.com>
2019-06-01 20:03:16 -0700

    updating dotenv config

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = env => {
     },
     plugins:[
       new DotenvPlugin({
-      path: path.resolve(__dirname, '/Portfoio/.env')
+      path: path.resolve(__dirname, '.env')
     }),
       new LodashModuleReplacementPlugin({
         'collections': true,
